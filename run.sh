2ff1591770f24f5f450a9cb4a9eb299cbe0e2256
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-01-15 00:35:35 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,24 +8,42 @@ cloudflare="run"
 scriptPathPihole="./Cloudflare-Gateway-Pihole"
 scriptPathPyhole="./Cloudflare-Gateway-Pyhole"
 
+# Function to run the appropriate script
+run_script() {
+    local script_path="$1"
+    local action="$2"
+    "$currentDirectory/$script_path" "$action"
+}
+
 # Grant execute permissions to the files
 chmod +x "$scriptPathPihole" "$scriptPathPyhole"
 
 # Get the current directory
 currentDirectory=$(pwd)
 
+# Function to display colored notifications
+notify() {
+    local color=$1
+    local message=$2
+    echo -e "\e[${color}m${message}\e[0m"
+}
+
 # Run the appropriate script based on mode and cloudflare
 case "$mode:$cloudflare" in
     normal:run)
-        "$currentDirectory/$scriptPathPihole" run
+        notify 32 "Running script..."
+        run_script "$scriptPathPihole" "run"
         ;;
     normal:leave)
-        "$currentDirectory/$scriptPathPihole" leave
+        notify 33 "Leaving script..."
+        run_script "$scriptPathPihole" "leave"
         ;;
     patch:run)
-        "$currentDirectory/$scriptPathPyhole" run
+        notify 32 "Running script..."
+        run_script "$scriptPathPyhole" "run"
         ;;
     patch:leave)
-        "$currentDirectory/$scriptPathPyhole" leave
+        notify 33 "Leaving script..."
+        run_script "$scriptPathPyhole" "leave"
         ;;
 esac
